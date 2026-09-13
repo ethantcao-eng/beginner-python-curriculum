16a8e487dc48a30208be76092d4f282630a12d09
ethantcao-eng <ethantcao@gmail.com>
2026-09-13 01:38:34 +0000

    hi

--- lesson1/classwork/practice.py
+++ lesson1/classwork/practice.py
@@ -1,24 +1,33 @@
 # Problem 1
 # Create a variable for your favorite number and print it.
-
+favorite_number=1
+print(favorite_number)
 
 
 # Problem 2
 # Create a variable for your height in meters and print it.
+height=1.3 
 
-
-
+print(height)
 # Problem 3
 # Create a variable for your favorite animal and print it in a full sentence.
 
 
-
+animal="koala" 
+print(animal)
 # Problem 4
 # Create two variables: one for your first name and one for your last name.
 # Print them on 2 separate lines.
 
-
+firstname="ethan" 
+lastname="cao" 
+print(firstname)
+print(lastname)
 
 # Problem 5
 # Create 3 variables: your first name, your age, and your favorite color.
 # Print them on the same line in a full sentence.
+firstname="ethan" 
+age=10
+color="blue"
+print("my favorite color is",color,"my name is",firstname,"my age is",age)
